db3a7624f9e84f30d4be89bc8cf5a447351b0530
Joe Downs <joedowns502@gmail.com>
2022-07-09 11:36:12 -0400

    wip

--- .github/workflows/github-labeler.py
+++ .github/workflows/github-labeler.py
@@ -16,17 +16,15 @@ from github import Github
 
 # ==============================================================================
 
-GITHUB_BASE_REF   = os.environ.get('GITHUB_BASE_REF')
-GITHUB_TOKEN      = os.environ.get('GITHUB_TOKEN')
+GITHUB_BASE_REF = os.environ.get('GITHUB_BASE_REF')
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 GITHUB_REPOSITORY = os.environ.get('GITHUB_REPOSITORY')
-GITHUB_REF        = os.environ.get('GITHUB_REF')
-PR_NUM            = os.environ.get('PR_NUM')
+PR_NUM = os.environ.get('PR_NUM')
 
 # Sanity check
 if (GITHUB_BASE_REF is None or
     GITHUB_TOKEN is None or
     GITHUB_REPOSITORY is None or
-    GITHUB_REF is None or
     PR_NUM is None):
     print("Error: this script is designed to run as a Github Action")
     exit(1)
@@ -37,26 +35,23 @@ if (GITHUB_BASE_REF is None or
 # the pull request, removes any matching the form "Target: {branch}" (if
 # {branch} is not the current target branch), and adds the correct label.
 def ensureLabels(pullRequest):
-    prBaseBranch = GITHUB_BASE_REF
-    prTargetLabels = list()
     needsLabel = True
+    targetPrefix = "Target: "
+    targetLabel = f"{targetPrefix}{GITHUB_BASE_REF}"
     for label in pullRequest.get_labels():
-        if label.name.startswith("Target:"):
-            prTargetLabels.append(label)
-    for targetLabel in prTargetLabels:
-        if targetLabel.name != f"Target: {prBaseBranch}":
-            pullRequest.remove_from_labels(targetLabel)
+        if label.name.startswith(targetPrefix):
+            if label.name == targetLabel:
+                needsLabel = False
+            else:
+                pullRequest.remove_from_labels(label)
     if needsLabel:
-        pullRequest.add_to_labels(f"Target: {prBaseBranch}")
+        pullRequest.add_to_labels(targetLabel)
     return
 
 # ==============================================================================
 
 g = Github(GITHUB_TOKEN)
 repo = g.get_repo(GITHUB_REPOSITORY)
-# Extract the PR number from GITHUB_REF
-print(GITHUB_REF)
-#match  = re.search("/(\d+)/", GITHUB_REF)
-pr_num = int(PR_NUM)
-pr     = repo.get_pull(pr_num)
+prNum = int(PR_NUM)
+pr = repo.get_pull(prNum)
 ensureLabels(pr)
